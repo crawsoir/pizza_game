884014fe682c8a450bd1e6fde6c073cba837cf0f
Crawsoir <werq10.fifty@gmail.com>
2023-04-28 01:30:23 -0400

    Remove test code

--- Battlescreen/BattleScreen.gd
+++ Battlescreen/BattleScreen.gd
@@ -8,6 +8,7 @@ class_name BattleScreen
 @onready var prebattle_timer = $PrebattleTimer
 
 @onready var live_dialogue = $LiveDialogueBox
+
 #would be nice to refactor this into a class object, so we dont have to manually reset it
 @onready var interactive_dialogue = $InteractiveDialogueBox
 
